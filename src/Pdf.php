41c756fbf2c525ecc0ac3fe246d1306bce7b7579
anil <webanilsidhu@gmail.com>
2018-12-04 23:50:21 +0530

    test master

---
+++ src/Pdf.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace NahidulHasan\Html2pdf;
+
+/**
+ * Laravel htm2pdf: convert html into pdf
+ *
+ * @package laravel-html2pdf
+ * @author Nahidul Hasan <nahidul.cse@gmail.com>
+ */
+class Pdf
+{
+
+    /**
+     * Convert html into pdf
+     *
+     * @param $input
+     * @return bool|string
+     */
+    public function generatePdf($input)
+    {
+
+        echo "<h1>next</h1>";
+        return "some next";
+
+    }
+
+}
